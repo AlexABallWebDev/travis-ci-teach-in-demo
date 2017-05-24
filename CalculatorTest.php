781400d2d142192ea6e3f7d6f84d9f994916324d
Alex Ball <aball6@mail.greenriver.edu>
2017-05-23 21:34:11 -0700

    Added test. Updated names and functionality to match those names.

--- CalculatorTest.php
+++ CalculatorTest.php
@@ -1,7 +1,13 @@
 <?php
 
 final class CalculatorTest extends PHPUnit_Framework_TestCase {
-    public function testZeroPlusZeroIsZero() {
-      $this->assertEquals(Calculator::add(1, 2) == 3);
+    public function test1Plus2Is3() {
+        //1 + 2 should equal 3
+        $this->assertEquals(Calculator::add(1, 2), 3);
+    }
+
+    public function test0Plus0Is0() {
+        //0 + 0 should equal 0
+        $this->assertEquals(Calculator::add(0, 0), 0);
     }
 }
