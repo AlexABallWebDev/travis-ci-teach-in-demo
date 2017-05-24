f23c8360b0dc4a5d8ba13a93a1de0ed7faa0e7b2
Alex Ball <aball6@mail.greenriver.edu>
2017-05-24 14:26:54 -0700

    Added more tests, including a zero test for each function.

--- CalculatorTest.php
+++ CalculatorTest.php
@@ -11,10 +11,22 @@ final class CalculatorTest extends PHPUnit_Framework_TestCase {
         $this->assertEquals(Calculator::add(0, 0), 0);
     }
 
+    public function test0PlusMinus5EqualsMinus5() {
+        $this->assertEquals(Calculator::add(0, -5), -5);
+    }
+
     //subtract tests
 
     public function test3Minus2Equals1() {
         $this->assertEquals(Calculator::subtract(3, 2), 1);
+    }
+
+    public function test0Minus0Equals0() {
+        $this->assertEquals(Calculator::add(0, 0), 0);
+    }
+
+    public function test0MinusMinus2EqualsMinus2() {
+        $this->assertEquals(Calculator::subtract(0, -2), -2);
     }
 
     //multiply tests
@@ -23,9 +35,17 @@ final class CalculatorTest extends PHPUnit_Framework_TestCase {
       $this->assertEquals(Calculator::multiply(1, 2), 2);
     }
 
+    public function test0Times0Equals0() {
+        $this->assertEquals(Calculator::add(0, 0), 0);
+    }
+
     //divide tests
 
     public function test3DividedBy3Equals1() {
       $this->assertEquals(Calculator::divide(3, 3), 1);
+    }
+
+    public function test0DividedBy0Equals0() {
+        $this->assertEquals(Calculator::add(0, 0), 0);
     }
 }
