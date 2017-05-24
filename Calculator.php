993eea246078322ae22114ae680fc37e268bb4b0
Alex Ball <aball6@mail.greenriver.edu>
2017-05-23 21:27:03 -0700

    Added addition function.

--- Calculator.php
+++ Calculator.php
@@ -1,3 +1,10 @@
 <?php
 
 //Calculator object. Test each function (addition, subtraction, etc.)
+class Calculator {
+    public static function add($num1, $num2) {
+      return $num1 + $num2;
+    }
+
+
+}
