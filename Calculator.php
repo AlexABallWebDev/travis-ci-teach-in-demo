a29ed9f8bede76983fea70fb821dda29fdb9f60c
Alex Ball <aball6@mail.greenriver.edu>
2017-05-23 21:37:35 -0700

    Added subtract function

--- Calculator.php
+++ Calculator.php
@@ -1,10 +1,11 @@
 <?php
 
-//Calculator object. Test each function (addition, subtraction, etc.)
 class Calculator {
     public static function add($num1, $num2) {
-      return $num1 + $num2;
+        return $num1 + $num2;
     }
 
-
+    public static function subtract($num1, $num2) {
+        return $num1 - $num2;
+    }
 }
