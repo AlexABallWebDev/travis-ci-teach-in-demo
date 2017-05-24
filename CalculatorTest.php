6d2731aafc8fd1560652d717ec61297c6fe99213
Alex Ball <aball6@mail.greenriver.edu>
2017-05-23 21:37:44 -0700

    Added test for subtract function

--- CalculatorTest.php
+++ CalculatorTest.php
@@ -1,13 +1,18 @@
 <?php
 
 final class CalculatorTest extends PHPUnit_Framework_TestCase {
-    public function test1Plus2Is3() {
-        //1 + 2 should equal 3
+    public function test1Plus2Equals3() {
+        // 1 + 2 should equal 3
         $this->assertEquals(Calculator::add(1, 2), 3);
     }
 
-    public function test0Plus0Is0() {
-        //0 + 0 should equal 0
+    public function test0Plus0Equals0() {
+        // 0 + 0 should equal 0
         $this->assertEquals(Calculator::add(0, 0), 0);
+    }
+
+    public function test3Minus2Equals1() {
+        // 3 - 2 should equal 1
+        $this->assertEquals(Calculator::subtract(3, 2), 1);
     }
 }
