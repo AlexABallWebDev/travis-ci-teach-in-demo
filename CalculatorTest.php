bc385e08b3bd156af5a51d628fd5839519498161
Alex Ball <aball6@mail.greenriver.edu>
2017-05-24 16:03:30 -0700

    Fixed indentation. Commented out function return statements so that tests would fail for screenshot.

--- CalculatorTest.php
+++ CalculatorTest.php
@@ -1,59 +1,59 @@
 <?php
 
 final class CalculatorTest extends PHPUnit_Framework_TestCase {
-    //add tests
+//add tests
 
-    public function test1Plus2Equals3() {
-        $this->assertEquals(Calculator::add(1, 2), 3);
-    }
+  public function test1Plus2Equals3() {
+    $this->assertEquals(Calculator::add(1, 2), 3);
+  }
 
-    public function test0Plus0Equals0() {
-        $this->assertEquals(Calculator::add(0, 0), 0);
-    }
+  public function test0Plus0Equals0() {
+    $this->assertEquals(Calculator::add(0, 0), 0);
+  }
 
-    public function test0PlusMinus5EqualsMinus5() {
-        $this->assertEquals(Calculator::add(0, -5), -5);
-    }
+  public function test0PlusMinus5EqualsMinus5() {
+    $this->assertEquals(Calculator::add(0, -5), -5);
+  }
 
-    //subtract tests
+  //subtract tests
 
-    public function test3Minus2Equals1() {
-        $this->assertEquals(Calculator::subtract(3, 2), 1);
-    }
+  public function test3Minus2Equals1() {
+    $this->assertEquals(Calculator::subtract(3, 2), 1);
+  }
 
-    public function test0Minus0Equals0() {
-        $this->assertEquals(Calculator::subtract(0, 0), 0);
-    }
+  public function test0Minus0Equals0() {
+    $this->assertEquals(Calculator::subtract(0, 0), 0);
+  }
 
-    public function test0MinusMinus2Equals2() {
-        $this->assertEquals(Calculator::subtract(0, -2), 2);
-    }
+  public function test0MinusMinus2Equals2() {
+    $this->assertEquals(Calculator::subtract(0, -2), 2);
+  }
 
-    //multiply tests
+  //multiply tests
 
-    public function test1Times2Equals2() {
-      $this->assertEquals(Calculator::multiply(1, 2), 2);
-    }
+  public function test1Times2Equals2() {
+    $this->assertEquals(Calculator::multiply(1, 2), 2);
+  }
 
-    public function test0Times0Equals0() {
-        $this->assertEquals(Calculator::multiply(0, 0), 0);
-    }
+  public function test0Times0Equals0() {
+    $this->assertEquals(Calculator::multiply(0, 0), 0);
+  }
 
-    public function testMinus1Times2EqualsMinus2() {
-        $this->assertEquals(Calculator::multiply(-1, 2), -2);
-    }
+  public function testMinus1Times2EqualsMinus2() {
+    $this->assertEquals(Calculator::multiply(-1, 2), -2);
+  }
 
-    //divide tests
+  //divide tests
 
-    public function test3DividedBy3Equals1() {
-      $this->assertEquals(Calculator::divide(3, 3), 1);
-    }
+  public function test3DividedBy3Equals1() {
+    $this->assertEquals(Calculator::divide(3, 3), 1);
+  }
 
-    public function test0DividedBy1Equals0() {
-        $this->assertEquals(Calculator::divide(0, 1), 0);
-    }
+  public function test0DividedBy1Equals0() {
+    $this->assertEquals(Calculator::divide(0, 1), 0);
+  }
 
-    public function testMinus6DividedBy2EqualsMinus3() {
-        $this->assertEquals(Calculator::divide(-6, 2), -3);
-    }
+  public function testMinus6DividedBy2EqualsMinus3() {
+    $this->assertEquals(Calculator::divide(-6, 2), -3);
+  }
 }
