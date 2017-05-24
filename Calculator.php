a31ef92d7571600c8b48e74301e615ff73575f56
Alex Ball <aball6@mail.greenriver.edu>
2017-05-24 08:09:21 -0700

    Added multiply and divide functions, and simple tests for each.

--- Calculator.php
+++ Calculator.php
@@ -8,4 +8,12 @@ class Calculator {
     public static function subtract($num1, $num2) {
         return $num1 - $num2;
     }
+
+    public static function multiply($num1, $num2) {
+      return $num1 * $num2;
+    }
+
+    public static function divide($num1, $num2) {
+      return $num1 / $num2;
+    }
 }
--- CalculatorTest.php
+++ CalculatorTest.php
@@ -1,18 +1,31 @@
 <?php
 
 final class CalculatorTest extends PHPUnit_Framework_TestCase {
+    //add tests
+
     public function test1Plus2Equals3() {
-        // 1 + 2 should equal 3
         $this->assertEquals(Calculator::add(1, 2), 3);
     }
 
     public function test0Plus0Equals0() {
-        // 0 + 0 should equal 0
         $this->assertEquals(Calculator::add(0, 0), 0);
     }
 
+    //subtract tests
+
     public function test3Minus2Equals1() {
-        // 3 - 2 should equal 1
         $this->assertEquals(Calculator::subtract(3, 2), 1);
+    }
+
+    //multiply tests
+
+    public function test1Times2Equals2() {
+      $this->assertEquals(Calculator::multiply(1, 2), 2);
+    }
+
+    //divide tests
+
+    public function test3DividedBy3Equals1() {
+      $this->assertEquals(Calculator::multiply(1, 2), 1);
     }
 }
