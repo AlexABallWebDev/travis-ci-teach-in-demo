dd4b4d00439483484c554276ad3a0e65ec9b257e
Ken Hang <khang@greenriver.edu>
2017-05-25 10:10:33 -0700

    breaking everything

--- Calculator.php
+++ Calculator.php
@@ -2,18 +2,18 @@
 
 class Calculator {
   public static function add($num1, $num2) {
-    return $num1 + $num2;
+    //return $num1 + $num2;
   }
 
   public static function subtract($num1, $num2) {
-    return $num1 - $num2;
+    //return $num1 - $num2;
   }
 
   public static function multiply($num1, $num2) {
-    return $num1 * $num2;
+    //return $num1 * $num2;
   }
 
   public static function divide($num1, $num2) {
-    return $num1 / $num2;
+    //return $num1 / $num2;
   }
 }
